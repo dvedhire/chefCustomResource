f3e0a7cd39c51e2f957af76e9ea5fb79408b8d37
Vedhire <vedhd@08f8bc638535.ant.amazon.com>
2022-06-08 17:14:46 +0100

    Initial Commit

---
+++ chef.py
@@ -0,0 +1,27 @@
+import boto3
+
+chefClient = boto3.client('opsworkscm')
+response = chefClient.create_server(
+        AssociatePublicIpAddress=True,
+        DisableAutomatedBackup=False,
+        Engine="ChefAutomate",
+        EngineModel="Single",
+        EngineVersion="12",
+        EngineAttributes=[
+            {
+                "Name": "UI_SUBNET_AZ",
+                "Value": "us-east-1d"
+            },
+        ],
+        BackupRetentionCount=10,
+        ServerName="sampleBoto3",
+        InstanceProfileArn="arn:aws:iam::<accountID>:instance-profile/aws-opsworks-cm-ec2-role",
+        InstanceType="m5.large",
+        KeyPair="vedhd",
+        PreferredMaintenanceWindow="Mon:22:00",
+        PreferredBackupWindow="06:00",
+        ServiceRoleArn="arn:aws:iam::<accountID>:role/service-role/aws-opsworks-cm-service-role",
+        SubnetIds=[
+            "subnet-c5cbaa2e",
+        ]
+    )
